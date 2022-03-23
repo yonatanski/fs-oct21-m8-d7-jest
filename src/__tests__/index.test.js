a36de87a90ce3314b0be605b0194ef8f0dbebe08
yonatanski <phpcodeyg@gmail.com>
2022-03-23 14:01:04 +0100

    Update index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -9,67 +9,69 @@ console.log(process.env.MONGO_URL)
 const client = supertest(app)
 
 describe("Testing the testing environment", () => {
-    test("should check that true is true", () => {
-        expect(true).toBe(true)
-    }) // it is an alias for test.
-    test("should check that 1 + 1 is 2", () => {
-        expect(1 + 1).toBe(2)
-    }) // it is an alias for test.
+  test("should check that true is true", () => {
+    expect(true).toBe(true)
+  }) // it is an alias for test.
+  test("should check that 1 + 1 is 2", () => {
+    expect(1 + 1).toBe(2)
+  }) // it is an alias for test.
 })
 
 describe("Testing the endpoints", () => {
+  beforeAll(async () => {
+    console.log("Before all tests...")
+    await mongoose.connect(process.env.MONGO_URL)
 
-    beforeAll(async () => {
-        console.log("Before all tests...")
-        await mongoose.connect(process.env.MONGO_URL)
+    console.log("Connected to Mongo")
+  })
 
-        console.log("Connected to Mongo")
-    })
+  it("should test that the test endpoint returns a success message", async () => {
+    const response = await client.get("/test")
+    expect(response.body.message).toBe("Test success")
+  })
 
+  //================================
 
-    it("should test that the test endpoint returns a success message", async () => {
-        const response = await client.get("/test")
-        expect(response.body.message).toBe("Test success")
-    })
+  const validProduct = {
+    name: "Test product",
+    price: 900,
+  }
 
-    const validProduct = {
-        name: "Test product",
-        price: 900
-    }
+  it("should test that the POST /products endpoint returns the newly created product", async () => {
+    const response = await client.post("/products").send(validProduct)
+    expect(response.status).toBe(201)
+    expect(response.body._id).toBeDefined()
 
-    it("should test that the POST /products endpoint returns the newly created product", async () => {
-        const response = await client.post("/products").send(validProduct)
-        expect(response.status).toBe(201)
-        expect(response.body._id).toBeDefined()
+    console.log(response.body)
+  })
 
-        console.log(response.body)
-    })
+  //================================
+  const invalidData = {
+    whatever: "something",
+  }
 
-    const invalidData = {
-        whatever: 'something'
-    }
+  it("should test that POST /products with INVALID data returns 400", async () => {
+    const response = await client.post("/products").send(invalidData)
+    expect(response.status).toBe(400)
+  })
+  //================================
 
-    it("should test that POST /products with INVALID data returns 400", async () => {
-        const response = await client.post("/products").send(invalidData)
-        expect(response.status).toBe(400)
-    })
+  let createdProductId
+  it("should test that the GET /products endpoint returns the product we just created", async () => {
+    const response = await client.get("/products")
+    expect(response.status).toBe(200)
+    expect(response.body.length).toBe(1)
 
-    let createdProductId
-    it("should test that the GET /products endpoint returns the product we just created", async () => {
-        const response = await client.get("/products")
-        expect(response.status).toBe(200)
-        expect(response.body.length).toBe(1)
+    createdProductId = response.body[0]._id
+  })
 
-        createdProductId = response.body[0]._id
-    })
+  // *****************
 
-    afterAll(async () => {
-        await mongoose.connection.dropDatabase()
-        await mongoose.connection.close()
+  //================================
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase()
+    await mongoose.connection.close()
 
-        console.log("Closed Mongo connection.")
-    })
-
-
+    console.log("Closed Mongo connection.")
+  })
 })
-
